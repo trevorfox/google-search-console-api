fa303b7a5cada3091c900003142d26cd7e30a4eb
Trevor <trevor@trevorfox.com>
2018-03-25 08:18:10 -0700

    first commit

---
+++ checker.py
@@ -0,0 +1,17 @@
+import requests
+import logging
+logging.basicConfig(filename='fixed.log',level=logging.DEBUG)
+
+
+# This function takes as arguments, script setup variables and record from the API query
+# As it stands, it calls the URL to see what status code is returned and prints it with the status code that Googlebot saw most recently
+# The commented block would then mark the URL as fixed if the URL returns a 200 response
+def check(SITE_URL, ERROR_CATEGORY, record):
+  verify = requests.get(SITE_URL + record['pageUrl'])
+  print (verify.url)
+  print (verify.status_code, record['responseCode'])
+
+  # if (verify.status_code == 200):
+  #     logging.debug(ERROR_CATEGORY + ', ' + check.url)
+  #     pprint(record)
+  #     fix = webmasters_service.urlcrawlerrorssamples().markAsFixed(siteUrl=SITE_URL, url=record['pageUrl'], category=ERROR_CATEGORY, platform='web').execute()
---
+++ webmaster.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import pickle
+from checker import check
+from google_auth_oauthlib.flow import InstalledAppFlow
+from apiclient.discovery import build
+
+# Script setup
+# Enter the URL for the site that you want to query as it is shown in Google Search Console
+# For this example, the ERROR_CATEGORY is used to query specific types of crawl errors
+SITE_URL = 'https://postmates.com/'
+ERROR_CATEGORY = 'notFound'
+
+# There are only two OAuth Scopes for the Google Search Console API
+# For the most part, all you will need is `.readonly` but if you want to modify data in Google Search Console,
+# you will need the second scope listed below
+# Read more: https://developers.google.com/webmaster-tools/search-console-api-original/v3/
+OAUTH_SCOPE = ('https://www.googleapis.com/auth/webmasters.readonly', 'https://www.googleapis.com/auth/webmasters')
+
+# Redirect URI for installed apps
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+# This is auth flow walks you through the Web auth flow the first time you run the script and stores the credentials in a file
+# Every subsequent time you run the script, the script will use the "pickled" credentials stored in config/credentials.pickle
+try:
+    credentials = pickle.load(open("config/credentials.pickle", "rb"))
+except (OSError, IOError) as e:
+    flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', scopes=OAUTH_SCOPE)
+    credentials = flow.run_console()
+    pickle.dump(credentials, open("config/credentials.pickle", "wb"))
+
+webmasters_service = build('webmasters', 'v3', credentials=credentials)
+
+# Query your Google Search Console data
+gsc_data = webmasters_service.urlcrawlerrorssamples().list(siteUrl=SITE_URL, category=ERROR_CATEGORY, platform='web').execute()
+records = gsc_data['urlCrawlErrorSample']
+
+# Iterate through all your URL records
+# Each record is in the form:
+"""
+{
+    'last_crawled': '2018-02-28T04:26:55.000Z',
+    'pageUrl': 'plan-check-kitchen-bar-los-angeles',
+    'urlDetails': {
+        'linkedFromUrls': [
+            'https://postmates.com/la/plan-check-kitchen-bar-los-angeles'
+        ]
+    }
+}
+"""
+
+for record in records:
+    check(SITE_URL, ERROR_CATEGORY, record)
